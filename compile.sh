c4db1fe805983d91f0e9d97bb4338cd7105a0c69
Francesco Palmisano <francesco.palmisano@gmail.com>
2017-02-28 22:30:42 +0100

    reconfigured kernel and saved as defconfig configured some gpio configured max clock for usdhc fixed issue in compile.sh

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@ export LDFLAGS=""
 make 
 export LOADADDR=0x10008000
 make uImage
-mkdir -f install_dir
+mkdir -p install_dir
 cp arch/arm/boot/uImage install_dir
 cp arch/arm/boot/dts/imx6q-marta.dtb install_dir
 make modules_install INSTALL_MOD_PATH=./install_dir/
